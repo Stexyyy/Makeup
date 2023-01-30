e3e3502c7abccd4171734f9492f5ffadd22f6d59
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2023-01-30 00:36:12 -0700

    Create if statements.py

---
+++ if statements.py
@@ -0,0 +1,13 @@
+ballin = input("hey do you like the legend of zelda? ")
+if ballin == "yes":
+  print("It's dangerous to go alone, take this!")
+else:
+  print("nuh uh")
+
+age = int(input("hey how old are you btw? "))
+if age < 13:
+  print("you're too young.")
+elif age >= 13 & age <= 18:
+  print("ask a parent")
+else:
+  print("welcome to the game!")
